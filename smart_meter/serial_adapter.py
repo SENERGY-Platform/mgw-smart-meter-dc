b78d2872f32b506b189e2b721e9959df8c45534f
y-du <42994541+y-du@users.noreply.github.com>
2020-12-15 08:23:23 +0100

    refactor

--- smart_meter/serial_adapter.py
+++ smart_meter/serial_adapter.py
@@ -38,6 +38,29 @@ class DataError(ReadError):
     pass
 
 
+def parse_data_telegram(data):
+    readings = dict()
+    readings_list = data.split('\r\n')
+    for reading in readings_list:
+        if '(' in reading:
+            key, value = reading.split('(')
+            if '*' in value:
+                value, unit = value.split('*')
+                unit = unit.replace(')', '')
+            else:
+                value = value.replace(')', '')
+                unit = None
+            try:
+                value = int(value)
+            except ValueError:
+                try:
+                    value = float(value)
+                except ValueError:
+                    pass
+            readings[key] = (value, unit)
+    return readings
+
+
 class SerialAdapter:
     init_telegram = '\x2f\x3f\x21\x0d\x0a'.encode()     # '/?! CR LF'
     ack_telegram = '\x06\x30\x35\x30\x0d\x0a'.encode()  # 'ACK 050 CR LF'
@@ -94,7 +117,7 @@ class SerialAdapter:
 
     def read(self):
         _, dt = self.__read()
-        return self.__parse_data_telegram(dt)
+        return parse_data_telegram(dt)
 
     def identify(self):
         mfr_id, dt = self.__read()
@@ -106,32 +129,10 @@ class SerialAdapter:
         mfr_id = mfr_id.split(".")
         if type(mfr_id) is list:
             mfr_id = mfr_id[0]
-        dt = self.__parse_data_telegram(dt)
+        dt = parse_data_telegram(dt)
         meter_ids = list()
         for key, val in dt.items():
             if key in ("C.1.0", "C.1.1", "0.0") and not str(val[0]).isspace() and not str(val[0]) in meter_ids:
                 meter_ids.append(str(val[0]))
         meter_ids.sort()
         return mfr_id, "".join(meter_ids)
-
-    def __parse_data_telegram(self, data):
-        readings = dict()
-        readings_list = data.split('\r\n')
-        for reading in readings_list:
-            if '(' in reading:
-                key, value = reading.split('(')
-                if '*' in value:
-                    value, unit = value.split('*')
-                    unit = unit.replace(')', '')
-                else:
-                    value = value.replace(')', '')
-                    unit = None
-                try:
-                    value = int(value)
-                except ValueError:
-                    try:
-                        value = float(value)
-                    except ValueError:
-                        pass
-                readings[key] = (value, unit)
-        return readings
